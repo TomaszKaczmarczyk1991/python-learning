776da4cf234eafe2b8e4ee24b537d5f245ef9460
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-10-25 15:43:12 +0200

    add not_operator.py

---
+++ not_operator.py
@@ -0,0 +1,9 @@
+print(not True) # False
+print(not False) # True
+
+str = "1991"
+print(str.isnumeric()) # True
+
+str = "Test"
+if not str.isnumeric():
+    print("Your string is not numeric!") # Your string is not numeric!
