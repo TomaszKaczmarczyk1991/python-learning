83a07616ee325299a97157a26e87cd9616336458
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-02 13:56:12 +0100

    refactor to function

--- is_even.py
+++ is_even.py
@@ -1,7 +1,13 @@
-num = input("Write a number: ")
-num = int(num)
+# num = input("Write a number: ")
+# num = int(num)
 
-if (num % 2 == 0):
-    print("Your number is even!")
-else:
-    print("Your number is odd!")
+# if (num % 2 == 0):
+#     print("Your number is even!")
+# else:
+#     print("Your number is odd!")
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
