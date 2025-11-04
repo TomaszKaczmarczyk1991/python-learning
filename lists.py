f99b6dede303a3f25dd9d921718aad5b2465eb6a
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-05 00:43:27 +0100

    add lists.py

---
+++ lists.py
@@ -0,0 +1,17 @@
+random_stuff = [True, "Hi", 444, []]
+
+premier_league_2025 = ["Chelsea", "Manchester City", "Brighton", "Aston Villa",
+                       "Arsenal", "Crystal Palace", "Brentford", "Newcastle",
+                       "Leeds", "Fulham", "West Ham", "Burnley",
+                       "Nottingham Forest", "Wolverhampton", "Tottenham",
+                       "Liverpool", "Sunderland", "Bournemouth", "Everton",
+                       "Manchester United"]
+
+print(len(premier_league_2025)) # 20
+
+
+pl = list("Premier League")
+print(pl) # ['P', 'r', 'e', 'm', 'i', 'e', 'r', ' ', 'L', 'e', 'a', 'g', 'u', 'e']
+
+teams = list(range(1, 21))
+print(teams) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
