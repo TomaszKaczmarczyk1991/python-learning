1d79e40a695d539f0e17a5abccbad5568b62e98b
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-10 23:08:49 +0100

    add dict.py

---
+++ dict.py
@@ -0,0 +1,20 @@
+pulp_fiction = {
+    "title": "Pulp Fiction",
+    "director": "Quentin Tarantino",
+    "year": 1994,
+    "imdb_rating": 8.8,
+    "main_cast": [
+        "John Travolta",
+        "Samuel L. Jackson",
+        "Uma Thurman",
+        "Bruce Willis",
+        "Ving Rhames",
+        "Harvey Keitel",
+    ],
+    "genres": ["Crime", "Drama"],
+    "mpaa_rating": "R",
+    "awards_summary": {
+        "oscars_nominations": ["Best Picture", "Best Actor (Travolta)", "Best Supporting Actor (Jackson)", "Best Original Screenplay"],
+        "wins": []
+    }
+}
