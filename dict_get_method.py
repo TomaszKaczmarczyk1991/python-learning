88cfbedd09dfdf4429502780415de6b2862a8dbd
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-12 05:24:02 +0100

    learn: dict get()

---
+++ dict_get_method.py
@@ -0,0 +1,10 @@
+synth_prices = {
+    "Behringer Model D": 189,
+    "KORG Minilogue XD": 549,
+    "Teenage Engineering OP-XY": 2222,
+    "Moog Muse": 3099,
+    "Moog Minimoog Model D 2022": 4999,
+    "Moog One": 12999
+}
+
+print(synth_prices.get("Moog One")) # 12999
