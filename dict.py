999a1f6fa5e3dbfdee0fd1345e06883a5fd5d3c8
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-12 05:27:24 +0100

    practice: adding and updating data

--- dict.py
+++ dict.py
@@ -31,4 +31,5 @@ pulp_fiction["is_great"] = True
 print(pulp_fiction) # {'title': 'Pulp Fiction', 'director': 'Quentin Tarantino', 'year': 1994, 'imdb_rating': 8.8, 'main_cast': ['John Travolta', 'Samuel L. Jackson', 'Uma Thurman', 'Bruce Willis', 'Ving Rhames', 'Harvey Keitel'], 'genres': ['Crime', 'Drama'], 'mpaa_rating': 'R', 'awards_summary': {'oscars_nominations': ['Best Picture', 'Best Actor (Travolta)', 'Best Supporting Actor (Jackson)', 'Best Original Screenplay'], 'wins': []}, 'is_great': True}
 pulp_fiction["imdb_rating"] += 1.0
 print(pulp_fiction["imdb_rating"]) # 9.8
-
+pulp_fiction["genres"].append("Comedy")
+print(pulp_fiction["genres"]) # ['Crime', 'Drama', 'Comedy']
