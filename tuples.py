0dcfb28073f0f730e0e8d461bc6da83e82f5632d
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-15 23:11:17 +0100

    add tuples.py

---
+++ tuples.py
@@ -0,0 +1,10 @@
+# tuples
+
+moogs = ("DFAM", "Subharmonicon", "Labyrinth", "Spectravox", "Mother 32")
+t = (True, False, 88, "Text", [])
+
+# empty_tuple = ()
+# empty_tuple = tuple()
+
+# single_tuple = "Single",
+# single_tuple = ("Single",)
