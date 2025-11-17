8313ece0cd9fff68435f8e510ec2bb94644abfd3
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-11-17 08:25:54 +0100

    add sets.py

---
+++ sets.py
@@ -0,0 +1,12 @@
+# Sets
+
+my_set = {1, 2, 3, 4}
+
+my_set.add(5)
+print(my_set) # {1, 2, 3, 4, 5}
+
+my_set.add(-4)
+print(my_set) # {1, 2, 3, 4, 5, -4}
+# The order of elements may appear random because sets are unordered
+
+print(len(my_set)) # 6
