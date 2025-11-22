91961ac3ecdf2981db518ce9f5bacb7ae4f997a1
Tomasz Kaczmarczyk <tomaszkaczmarczyk@tuta.io>
2025-11-22 05:06:43 +0100

    learn: intersection, union, difference

--- sets.py
+++ sets.py
@@ -20,3 +20,16 @@ print(my_set) # {1, 2, 4, 5}
 
 my_set.clear()
 print(my_set) # set()
+
+# Intersection
+first_set = {1, 2, 3, 5}
+second_set = {2, 3, 4, 6}
+
+print(first_set & second_set) # {2, 3}
+
+# Union
+print(first_set | second_set) # {1, 2, 3, 4, 5, 6}
+
+# Difference
+print(first_set - second_set) # {1, 5}
+print(second_set - first_set) # {4, 6}
