efdbc42417e5d71147701918800c680016cba56c
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-12-01 04:12:19 +0100

    learn: class attributes

--- oop.py
+++ oop.py
@@ -1,8 +1,11 @@
 class Synth:
+    instrument_type = ["Electronic musical instrument", "Electrophone"]
+    num_synths = 0
     def __init__(self, name, brand, presets):
         self.name = name
         self.brand = brand
         self.presets = []
+        Synth.num_synths += 1
     def add_preset(self, preset):
         if preset not in self.presets:
             self.presets.append(preset)
@@ -25,3 +28,9 @@ print(subsequent25.presets) # ['Classic Lead']
 subsequent25.add_preset('Classic Lead') # A preset with that name already exists!
 subsequent25.add_preset('Sub Bass')
 print(subsequent25.presets) # ['Classic Lead', 'Sub Bass']
+
+print(subsequent25.instrument_type) # ['Electronic musical instrument', 'Electrophone']
+print(Synth.num_synths) # 3
+
+one = Synth('One', 'Moog', [])
+print(Synth.num_synths) # 4
